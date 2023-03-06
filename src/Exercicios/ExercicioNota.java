343b4169a36d8c9c7fc3c640b1fd04781393d8a8
PaolaBarbosa23 <pahbracho23@gmail.com>
2023-03-06 15:56:44 -0300

    primeiro commit

---
+++ src/Exercicios/ExercicioNota.java
@@ -0,0 +1,19 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class ExercicioNota {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while(nota < 0 | nota > 10){
+            System.out.println("Nota inválida! Digite novamente: ");
+            nota = scan.nextInt();
+        }
+    }
+}
